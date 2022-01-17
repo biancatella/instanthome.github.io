7a25eab5c5f5c192f75c49c083461d3ff081379f
Bianca Gueldini Tella <biancagt@MMB026072CPS.local>
2022-01-16 23:09:31 -0300

    highlight effect

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,9 +1,47 @@
 $(document).ready(function() {
 
+    //CHECK IF THE ELEMENT IS VISIBLE ON VIEWPORT
+    $.fn.isInViewport = function() {
+        var elementTop = $(this).offset().top;
+        var elementBottom = elementTop + $(this).outerHeight();
+
+        var viewportTop = $(window).scrollTop();
+        var viewportBottom = viewportTop + $(window).height();
+
+        return elementBottom > viewportTop && elementTop < viewportBottom;
+    };
+
+    //HIGHLIGT ANIMATION
+    $(window).on('resize scroll', function() {
+        if ($('.highlight-text').isInViewport()) {
+            $(".highlight-text").addClass('animate');
+        }
+    });
 
 
 
 
+
+
+
+
+    // if (window.matchMedia('(min-width: 770px)').matches) {
+    //     $(window).scroll(function() {
+
+    //         if ($(window).scrollTop() > 100) {
+    //             console.log('scroll down');
+    //             // $(".navbar").css({ "background-color": "rgba(255, 255, 255, 1)" });
+    //             $(".highlight-text").addClass('animate');
+
+    //         }
+
+    //         if ($(window).scrollTop() < 100) {
+    //             console.log('scroll up');
+    //             // $(".navbar").css({ "background-color": "transparent" });
+    //             $(".highlight-text").removeClass('animate');
+    //         }
+    //     })
+    // }
 
 
     // SCROLL CHANGES MENU DESKTOP//
